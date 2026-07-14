9d4b7b1430c9b83cbc70a6bd5a9a51f32d4b3f19
Sumanth <sumanthpoojary965@gmail.com>
2026-07-14 20:23:47 +0530

    Time: 0 ms (100%), Space: 42.8 MB (78.75%) - LeetHub

---
+++ 0551-student-attendance-record-i/0551-student-attendance-record-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkRecord(String s) {
+        int Count_A=0;
+        int Count_L=0;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='A'){
+                Count_A++;
+                Count_L=0;
+            }
+            else if(ch=='L'){
+                Count_L++;
+            }else{
+                Count_L=0;
+            }
+            if(Count_A>=2){
+            return false;
+        }else if(Count_L>=3){
+            return false;
+        }
+        }return true;
+        
+    }
+}
