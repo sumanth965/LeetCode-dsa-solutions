f071f8b548bb616fd7b936d7a0944b597de0cb72
Sumanth <sumanthpoojary965@gmail.com>
2026-07-23 18:54:09 +0530

    Time: 3 ms (41.36%), Space: 43.3 MB (36.04%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,12 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        int charSum = 0;
+        for (int i = 0; i < t.length(); ++i) {
+            charSum += t.charAt(i);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            charSum -= s.charAt(i);
+        }
+        return (char) charSum;
+    }
+}
