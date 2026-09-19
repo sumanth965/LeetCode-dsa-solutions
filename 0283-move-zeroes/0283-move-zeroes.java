cd6b7409884810e0fa978848ba91c212640551d4
Sumanth <sumanthpoojary965@gmail.com>
2026-09-19 20:54:49 +0530

    Time: 2 ms (91.67%), Space: 47.4 MB (93%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
-class Solution { 
-    public void moveZeroes(int[] nums) { 
-        int k = 0; 
-        
-        for (int i = 0; i < nums.length; i++) { 
-            if (nums[i] != 0) { 
-                nums[k] = nums[i]; 
-                k++; 
-            } 
-        } 
-        
-        while (k < nums.length) { 
-            nums[k] = 0; 
-            k++; 
-        } 
-    } 
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // int arr[] =new int[nums.length];
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        while(nums.length>k){
+                nums[k]=0;
+                k++;
+        }
+    }
 }
